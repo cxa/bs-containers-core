9a9a9cfac3ddbb9bc8a18f46977ff6d5213ec9b7
CHEN Xian-an <xianan.chen@gmail.com>
2017-05-28 01:48:16 +0800

    Make CCFun and CCString great again

--- mksrc.sh
+++ mksrc.sh
@@ -5,11 +5,13 @@ git submodule update --init
 cd ocaml-containers
 git checkout 1.2
 echo "Copying sources..."
+rm -rf ../src/*
 cp src/core/*.ml ../src
 cp src/core/*.mli ../src
 cd ../src
 rm *Labels.ml* # which bsb can not compile
-rm *.cppo.ml*
+rm CCIO.ml* # not needed
+for i in *cppo.ml; do mv $i ${i/cppo./}; done
 sed -i '' "/Labels/d" containers.ml
 echo "Copying LICENSE..."
 cp ../ocaml-containers/LICENSE ./
